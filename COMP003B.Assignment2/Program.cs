5cac50c1bcd47bc39616b0c35675d10ee78df01a
adodoCoder <alavropulido@my.whccd.edu>
2026-03-25 13:33:18 -0700

    Add project files.

---
+++ COMP003B.Assignment2/Program.cs
@@ -0,0 +1,47 @@
+/*
+ * Author: [Alvaro Pulido]
+ * Course: COMP003B ASP.NET Core
+ * Faculty: Jonathan Cruz
+ * Purpose: Coding Assignment MVC
+ */
+    namespace COMP003B.Assignment2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseMiddleware<COMP003B.Assignment2.Middleware.RequestTrackerMiddleware>();
+
+            app.UseWelcomePage("/welcome");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
