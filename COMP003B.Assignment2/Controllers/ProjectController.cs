1ceda96776d5f8916b15a842caf022a12bf06d78
adodoCoder <alavropulido@my.whccd.edu>
2026-03-25 13:53:42 -0700

    file name controler was added

---
+++ COMP003B.Assignment2/Controllers/ProjectController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers
+{
+    public class ProjectController : Controller
+    {
+        [HttpGet]
+        public IActionResult Current()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Completed()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Ideas()
+        {
+            return View();
+        }
+    }
+}
